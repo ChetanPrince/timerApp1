9b67e8b489a7892d690cb9d4bb4e3a7c80510db5
ChetanPrince <saini.rajvir2@gmail.com>
2024-08-09 23:49:40 +0200

    button script

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const button = document.getElementById("submit");
+button.addEventListener("click", ()=>{
+
+
+    const input = document.querySelector(".input");
+    const output = document.querySelector("#output");
+    output.innerHTML = `<p>The list is sorted.</p>`;
+    
+})
