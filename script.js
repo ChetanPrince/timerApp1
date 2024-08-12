9e8e4092dfa52e0504548a0612851bb7d8bf4178
ChetanPrince <saini.rajvir2@gmail.com>
2024-08-12 02:38:10 +0200

    added basic functionality

--- script.js
+++ script.js
@@ -6,14 +6,39 @@ button.addEventListener("click", ()=>{
     containers.forEach(container=>{
 
         container.classList.toggle("hide");
-    })
-    const button = document.querySelector(".startQuiz");
-    if(button.innerText === "Start Quiz"){
-        button.innerText = "Next";
-    }
-    else{
-        button.innerText = "Start Quiz";
-    }
-  
+        if(container.classList != "hide"){
+
+            if (!container.querySelector(".next")) {
+                container.innerHTML += `<button class="next" onClick="next()">Next</button>`;
+            }
+        }
+    });
+   button.classList.add("hide");
+
+});
+
+function next() {
+    const questions = document.querySelectorAll(".questions");
     
-})
+    let currentQuestionIndex = -1;
+
+    // Find the currently visible question
+    questions.forEach((question, index) => {
+        if (!question.classList.contains("hide")) {
+            currentQuestionIndex = index;
+            question.classList.add("hide"); // Hide the current question
+        }
+    });
+
+    // Determine the index of the next question
+    let nextQuestionIndex = currentQuestionIndex + 1;
+
+    // If we are at the last question, hide the .next button and stop
+    if (nextQuestionIndex < questions.length) {
+        questions[nextQuestionIndex].classList.remove("hide");
+    } else {
+        const nextButtons = document.querySelectorAll(".next");
+        nextButtons.forEach(nextButton => {
+            nextButton.classList.add("hide");
+        });
+}}
